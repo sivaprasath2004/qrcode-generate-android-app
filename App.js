1eee3a6b74c706af5fb4fa401e7d3884b8001639
sivaprasath2004 <prasathsiva2004@gmail.com>
2024-02-21 07:08:57 +0530

    scan Tab

--- App.js
+++ App.js
@@ -1,36 +1,52 @@
 import { useState } from 'react';
-import { View,TextInput,StatusBar,TouchableOpacity,Text } from 'react-native';
-import QRCode from 'react-native-qrcode-svg';
+import Qrcode from './screens/Qrcode';
+import Scan from './screens/scan';
+import {View,Text} from 'react-native'
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons ,MaterialCommunityIcons} from '@expo/vector-icons';
 export default function App() {
-  const [qrcodevalue,setQrcodevalue]=useState({text:'',generate:false})
+  const Tab=createBottomTabNavigator()
   return (
-    <>
-    <View style={{width:'100%',height:'100%',gap:20,backgroundColor:'black'}}>
-      <StatusBar barStyle='white' backgroundColor='black' />
-      <Text style={{color:'white',fontWeight:900,fontSize:40,padding:20}}>Home</Text>
-     <View style={{width:'100%',height:'100%',gap:40,borderTopRightRadius:40,borderTopLeftRadius:40,justifyContent:'center',alignItems:'center',backgroundColor:'white'}}>
-     <View style={{flexDirection:'row',justifyContent:'center',gap:20,alignItems:'center'}}>
-     <View style={{height:40,width:200}}>
-     <TextInput
-     style={{height:'100%',width:'90%'}}
-     placeholder='Enter the command'
-     value={qrcodevalue.text}
-     onChangeText={(text)=>setQrcodevalue(pre=>({...pre,text:text,generate:false}))}
-     /></View>
-     <TouchableOpacity 
-     onPress={()=>setQrcodevalue(pre=>({...pre,generate:true,second:qrcodevalue.text}))}
-     style={{backgroundColor:'black',height:40,width:90,borderRadius:15,justifyContent:'center',alignItems:'center'}}><Text style={{color:'white',fontWeight:900}}>Convert</Text></TouchableOpacity>
-     </View>
-    <QRCode
-     value={qrcodevalue.second===undefined || qrcodevalue.second===""?'Enter SomeOne':qrcodevalue.second}
-     size={300}
-     bgColor='white'
-     fgColor='black'
-     />
-     <Text>{qrcodevalue.second===undefined || qrcodevalue.second===""?'Enter SomeOne':qrcodevalue.second}</Text>
-
-</View> 
-    </View>
-    </>
+   <NavigationContainer>
+    <Tab.Navigator screenOptions={
+      {
+        tabBarShowLabel:false,
+        headerShown:false,
+        tabBarStyle:{
+          position:'absolute',
+          bottom:0,
+          right:0,
+          elevation:0,
+          height:60,
+          backgroundColor:'white'
+        }
+      }
+    }>
+      <Tab.Screen name='Scan' component={Scan}
+      options={{
+        tabBarIcon:({focused})=>{
+        return (
+          <View style={{justifyContent:'center',alignItems:'center'}}>
+          <MaterialCommunityIcons name="data-matrix-scan" size={24} color={focused?'red':'black'} />
+          <Text style={{fontSize:20}}>Qrcode</Text>
+           </View>
+        )
+      }
+      }}
+       />
+      <Tab.Screen name='Qrcode' component={Qrcode}
+       options={{
+        tabBarIcon:({focused})=>{
+        return (
+           <View style={{justifyContent:'center',alignItems:'center'}}>
+           <Ionicons name="qr-code" size={24} color={focused?'red':'black'} />
+           <Text style={{fontSize:20}}>Qrcode</Text>
+            </View>
+        )
+      }
+      }} />
+    </Tab.Navigator>
+   </NavigationContainer>
 )
   }
---
+++ screens/Qrcode.js
@@ -0,0 +1,36 @@
+import React,{useState} from 'react'
+import QRCode from 'react-native-qrcode-svg';
+import { View,TextInput,StatusBar,TouchableOpacity,Text } from 'react-native';
+const Qrcode = () => {
+  const [qrcodevalue,setQrcodevalue]=useState({text:'',generate:false})
+  return (
+    <View style={{width:'100%',height:'100%',gap:20,backgroundColor:'black'}}>
+      <StatusBar barStyle='white' backgroundColor='black' />
+      <Text style={{color:'white',fontWeight:900,fontSize:40,padding:20}}>Home</Text>
+     <View style={{width:'100%',height:'100%',gap:40,borderTopRightRadius:40,borderTopLeftRadius:40,justifyContent:'center',alignItems:'center',backgroundColor:'white'}}>
+     <View style={{flexDirection:'row',justifyContent:'center',gap:20,alignItems:'center'}}>
+     <View style={{height:40,width:200}}>
+     <TextInput
+     style={{height:'100%',width:'90%'}}
+     placeholder='Enter the command'
+     value={qrcodevalue.text}
+     onChangeText={(text)=>setQrcodevalue(pre=>({...pre,text:text,generate:false}))}
+     /></View>
+     <TouchableOpacity 
+     onPress={()=>setQrcodevalue(pre=>({...pre,generate:true,second:qrcodevalue.text}))}
+     style={{backgroundColor:'black',height:40,width:90,borderRadius:15,justifyContent:'center',alignItems:'center'}}><Text style={{color:'white',fontWeight:900}}>Generate</Text></TouchableOpacity>
+     </View>
+    <QRCode
+     value={qrcodevalue.second===undefined || qrcodevalue.second===""?'Enter SomeOne':qrcodevalue.second}
+     size={300}
+     bgColor='white'
+     fgColor='black'
+     />
+     <Text>{qrcodevalue.second===undefined || qrcodevalue.second===""?'Enter SomeOne':qrcodevalue.second}</Text>
+
+</View> 
+    </View>
+  )
+}
+
+export default Qrcode
