ecd280f1a53d46a83b48cfac150172a693cee45e
sivaprasath2004 <prasathsiva2004@gmail.com>
2024-02-21 21:42:14 +0530

    settings Page

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import Qrcode from './screens/Qrcode';
-import Scan from './screens/scan';
+import Settings from './screens/Settings';
 import {View,Text} from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -18,30 +18,30 @@ export default function App() {
           bottom:0,
           right:0,
           elevation:0,
-          height:60,
+          height:40,
           backgroundColor:'white'
         }
       }
     }>
-      <Tab.Screen name='Scan' component={Scan}
+      <Tab.Screen name='Qrcode' component={Qrcode}
       options={{
         tabBarIcon:({focused})=>{
+          
         return (
           <View style={{justifyContent:'center',alignItems:'center'}}>
-          <MaterialCommunityIcons name="data-matrix-scan" size={24} color={focused?'red':'black'} />
-          <Text style={{fontSize:20}}>Qrcode</Text>
+          <Ionicons name="qr-code" size={24}
+          color={focused?'red':'black'} />
            </View>
         )
       }
       }}
        />
-      <Tab.Screen name='Qrcode' component={Qrcode}
+      <Tab.Screen name='Settings' component={Settings}
        options={{
         tabBarIcon:({focused})=>{
         return (
-           <View style={{justifyContent:'center',alignItems:'center'}}>
-           <Ionicons name="qr-code" size={24} color={focused?'red':'black'} />
-           <Text style={{fontSize:20}}>Qrcode</Text>
+           <View style={{justifyContent:'center',alignItems:'center'}} >
+           <Ionicons name="settings" size={24}  color={focused?'red':'black'} />
             </View>
         )
       }
--- screens/scan.js
+++ screens/scan.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, StyleSheet, Button,StatusBar } from 'react-native';
 import { Camera } from 'expo-camera';
 
 export default function Scan() {
@@ -8,12 +8,12 @@ export default function Scan() {
 
   useEffect(() => {
     const getCameraPermissions = async () => {
-      const { status } = await Camera.requestPermissionsAsync();
+      const { status } = await Camera.requestCameraPermissionsAsync()
       setHasPermission(status === 'granted');
     };
 
     getCameraPermissions();
-  }, []);
+  },[]);
 
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
@@ -29,19 +29,21 @@ export default function Scan() {
 
   return (
     <View style={styles.container}>
+      <StatusBar barStyle='white' backgroundColor='black' />
+      <Text style={{fontSize:30,fontWeight:900,paddingLeft:10,color:'white',marginTop:20}}>Hello</Text>
+      <View style={{height:'100%',justifyContent:'center',alignItems:'center',width:'100%'}}>
       <Camera
         onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
         style={StyleSheet.absoluteFillObject}
       />
       {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
+    height:'100%',width:'100%',backgroundColor:'black'
   },
 });
